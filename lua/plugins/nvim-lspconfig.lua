191310d30ba8d919582380eb5edb3f67aa5992f7
Trevor Grieger <tgrieger@localhost-live.attlocal.net>
2024-06-01 22:01:00 -0400

    Initial customizations

---
+++ lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,10 @@
+return {
+  "neovim/nvim-lspconfig",
+  config = function()
+    local lspconfig = require("lspconfig")
+    lspconfig.lua_ls.setup({})
+    lspconfig.gleam.setup({})
+
+    vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+  end,
+}
