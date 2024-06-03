8bac63f0d0093d72d2a68257cfe9669a2739a1b6
Trevor Grieger <tgrieger@localhost-live.attlocal.net>
2024-06-03 01:43:28 -0400

    Add dotnet lsp

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,12 +1,5 @@
 return {
   "neovim/nvim-lspconfig",
-  -- config = function()
-  --   local lspconfig = require("lspconfig")
-  --   lspconfig.lua_ls.setup({})
-  --   lspconfig.gleam.setup({})
-  --
-  --   vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-  -- end,
   opts = {
     servers = {
       gleam = {},
