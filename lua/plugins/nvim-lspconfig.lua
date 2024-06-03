5e4840ccc31a5da86f5fdaa87752280517b0e712
Trevor Grieger <tgrieger@localhost-live.attlocal.net>
2024-06-02 23:03:42 -0400

    Setup keymaps and lsp

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,10 +1,16 @@
 return {
   "neovim/nvim-lspconfig",
-  config = function()
-    local lspconfig = require("lspconfig")
-    lspconfig.lua_ls.setup({})
-    lspconfig.gleam.setup({})
-
-    vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-  end,
+  -- config = function()
+  --   local lspconfig = require("lspconfig")
+  --   lspconfig.lua_ls.setup({})
+  --   lspconfig.gleam.setup({})
+  --
+  --   vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+  -- end,
+  opts = {
+    servers = {
+      gleam = {},
+      lua_ls = {},
+    },
+  },
 }
