cd310844d1aa7795ce8e7cce583344af21312da8
Trevor Grieger <trevorpsn@gmail.com>
2025-01-14 00:30:28 -0500

    Initial changes
    This allows control+backspace to delete the previous word and sets
    powershell as the default shell on windows

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,5 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.api.nvim_set_keymap("i", "<C-H>", "<C-W>", { noremap = true })
+vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
