c7f7d06a788130139bde54c7cae1ebcda52edf71
Trevor Grieger <trevorpsn@gmail.com>
2025-01-14 01:31:43 -0500

    Switch terminal to powershell and use toggleterm

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,3 +3,4 @@
 -- Add any additional keymaps here
 vim.api.nvim_set_keymap("i", "<C-H>", "<C-W>", { noremap = true })
 vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+vim.keymap.set({ "n", "t" }, "<C-_>", "<cmd>ToggleTerm direction=float<cr>", { remap = true })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,20 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.g.snacks_animate = false
+
 if vim.loop.os_uname().sysname == "Windows_NT" then
-  vim.opt.shell = "powershell.exe"
+  local powershell_options = {
+    shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell",
+    shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+    shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+    shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+    shellquote = "",
+    shellxquote = "",
+  }
+
+  for option, value in pairs(powershell_options) do
+    vim.opt[option] = value
+  end
 end
